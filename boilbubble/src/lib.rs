92b338d3ef16c5c3101acdba0c6f813582c09427
caleb0109 <caleb0109@outlook.com>
2025-12-10 18:46:17 -0500

    track loops infinitely

--- boilbubble/src/lib.rs
+++ boilbubble/src/lib.rs
@@ -38,79 +38,75 @@ impl GameState {
     }
     pub fn update(&mut self) {
         let mut select: (f32,f32) = (0.0,0.0);
+        let mut select2: (f32,f32) = (0.0,0.0);
         let m = pointer::world();
-        //for every 1.5 seconds that pass, the next item on the track will appear
-        if time::tick() % 90 == 0 && self.trackPrint <= 9 && self.day > 0{
+        //for every 5/6 of a second that pass, the next item on the track will appear
+        //is not perfect for sure, but visually works for now
+        //will look into further ways to optimize
+        if time::tick() % 50 == 0 && self.trackPrint <= 7 && self.day > 0{
             self.trackPrint += 1;
         }
         //for loop to create the track
         for n in 0..self.trackPrint {
-            select = self.trackList.ingredPos[n].0.check(select);
+            select = self.trackList.ingredPos1[n].0.check(select);
+            select2 = self.trackList.ingredPos2[n].0.check(select2);
             //checks if the track item is at the end of the opposite side from start
-            if !self.trackList.trackPos[n].3 {
+            if !self.trackList.trackPos1[n].2 {
                 //if the track item has yet to reach the max height and is on starting side
-                if self.trackList.trackPos[n].1 <= 105.0 && !self.trackList.trackPos[n].2{
-                    self.trackList.trackPos[n].1 += 0.175;
-                    self.trackList.trackPos[n].0 += 0.625;
-                }
-                //if track item reaches max height
-                if self.trackList.trackPos[n].1 >= 105.0 {
-                    self.trackList.trackPos[n].2 = true;
-                    self.trackList.trackPos[n].1 -= 0.175;
-                    self.trackList.trackPos[n].0 += 0.625;
-                }
-                //if track item reached max height, now ascending and making sure it doesn't go below starting height
-                if self.trackList.trackPos[n].1 <= 105.0 && self.trackList.trackPos[n].2 && self.trackList.trackPos[n].1 >= 70.175{
-                    self.trackList.trackPos[n].1 -= 0.175;
-                    self.trackList.trackPos[n].0 += 0.625;
+                if self.trackList.trackPos1[n].0 <= 248.0 && !self.trackList.trackPos1[n].2{
+                    self.trackList.trackPos1[n].0 += 0.625;
                 }
                 //if track item reaches end of sceen on opposite side
-                if self.trackList.trackPos[n].0 >= 248.0 {
-                    self.trackList.trackPos[n].3 = true;
-                    self.trackList.trackPos[n].2 = false;
-                    self.trackList.trackPos[n].1 = 70.0;
-                
+                if self.trackList.trackPos1[n].0 >= 248.0 {
+                    self.trackList.trackPos1[n].2 = true;
                 }
             }
             //checks if the track item is on the way back to starting side
-            if self.trackList.trackPos[n].3{
+            if !self.trackList.trackPos2[n].2{
                 //if the track item reaches min height and is on starting side
-                if self.trackList.trackPos[n].1 >= 35.0 && !self.trackList.trackPos[n].2{
-                    self.trackList.trackPos[n].1 -= 0.175;
-                    self.trackList.trackPos[n].0 -= 0.625;
-                }
-                //if track item reaches min height
-                if self.trackList.trackPos[n].1 <= 35.0 {
-                    self.trackList.trackPos[n].2 = true;
-                    self.trackList.trackPos[n].1 += 0.175;
-                    self.trackList.trackPos[n].0 -= 0.625;
+                if self.trackList.trackPos2[n].0 >= 0.0 && !self.trackList.trackPos1[n].2{
+                    self.trackList.trackPos2[n].0 -= 0.625;
                 }
                 //if track item reached min height, now descending and making sure it doesn't go above starting height
-                if self.trackList.trackPos[n].1 >= 35.0 && self.trackList.trackPos[n].2 && self.trackList.trackPos[n].1 <= 70.175{
-                    self.trackList.trackPos[n].1 += 0.175;
-                    self.trackList.trackPos[n].0 -= 0.625;
+                if self.trackList.trackPos2[n].0 <= 2.5{
+                    self.trackList.trackPos2[n].2 = true;
                 }
-
-                //will add code for restarting track so is endless loop of rotation
-            
             }
             
             //if the ingredient isn't being held, then set its position to the track position
-            if !self.trackList.ingredPos[n].0.action {
-                self.trackList.ingredPos[n].0.hitbox.0 = self.trackList.trackPos[n].0;
-                self.trackList.ingredPos[n].0.hitbox.1 = self.trackList.trackPos[n].1;
+            if !self.trackList.ingredPos1[n].0.action {
+                self.trackList.ingredPos1[n].0.hitbox.0 = self.trackList.trackPos1[n].0;
+                self.trackList.ingredPos1[n].0.hitbox.1 = self.trackList.trackPos1[n].1;
+            }
+            if !self.trackList.ingredPos2[n].0.action {
+                self.trackList.ingredPos2[n].0.hitbox.0 = self.trackList.trackPos2[n].0;
+                self.trackList.ingredPos2[n].0.hitbox.1 = self.trackList.trackPos2[n].1;
             }
             //if the pointer releases the ingredient, ingredient is not active
             if m.just_released() {
-                self.trackList.ingredPos[n].0.action = false;
+                self.trackList.ingredPos1[n].0.action = false;
+                self.trackList.ingredPos2[n].0.action = false;
             }
-            //if it has a specific name, then draw rect to see difference
-            if self.trackList.ingredPos[n].1.name == "Sugar" {
-                self.trackList.ingredPos[n].0.tempDraw();
+            //if it has a specific name and , then draw rect to see difference
+            if self.trackList.ingredPos1[n].1.name == "Sugar" && !self.trackList.trackPos1[n].2{
+                self.trackList.ingredPos1[n].0.tempDraw();
             }
-            //will print track plate always, regardless of ingredient
-            circ!(x = self.trackList.trackPos[n].0, y = self.trackList.trackPos[n].1, d=8, color = 0x32CD32ff);
-            
+            if self.trackList.ingredPos2[n].1.name == "Sugar" && !self.trackList.trackPos2[n].2{
+                self.trackList.ingredPos2[n].0.tempDraw();
+            }
+            //if the track item reaches the end of the screen, then reset it to start
+            if !self.trackList.trackPos1[n].2 {
+                circ!(x = self.trackList.trackPos1[n].0, y = self.trackList.trackPos1[n].1, d=8, color = 0x32CD32ff);
+            } else if self.trackList.trackPos1[n].2 {
+                self.trackList.trackPos1[n].2 = false;
+                self.trackList.trackPos1[n].0 = 0.0;
+            }
+            if !self.trackList.trackPos2[n].2 {
+                circ!(x = self.trackList.trackPos2[n].0, y = self.trackList.trackPos2[n].1, d=8, color = 0x32CD32ff);
+            } else if self.trackList.trackPos2[n].2 {
+                self.trackList.trackPos2[n].2 = false;
+                self.trackList.trackPos2[n].0 = 250.0;
+            }
         }
 
         //check to see if day continue button is pressed or not
@@ -121,10 +117,13 @@ impl GameState {
                 self.day += 1;
                 self.uibuttons[0].action = false;
                 self.trackPrint = 0;
-                for n in 0..10 {
-                    self.trackList.trackPos[n] = (0.0,70.0,false,false);
-                    self.trackList.ingredPos[n].0.hitbox.0 = 0.0;
-                    self.trackList.ingredPos[n].0.hitbox.1 = 70.0;
+                for n in 0..8 {
+                    self.trackList.trackPos1[n] = (0.0,100.0,false);
+                    self.trackList.trackPos2[n] = (250.0,30.0,false);
+                    self.trackList.ingredPos1[n].0.hitbox.0 = 0.0;
+                    self.trackList.ingredPos1[n].0.hitbox.1 = 100.0;
+                    self.trackList.ingredPos2[n].0.hitbox.0 = 250.0;
+                    self.trackList.ingredPos2[n].0.hitbox.1 = 30.0;
                 }   
             }
             self.uibuttons[n].tempDraw();
--- boilbubble/src/track.rs
+++ boilbubble/src/track.rs
@@ -6,8 +6,10 @@ use crate::UIButtons::UIButtons::UIButton;
 #[turbo::serialize]
 pub struct Track {
     //data of track and ingredients on track
-    pub trackPos: [(f32,f32,bool,bool); 10],
-    pub ingredPos: [(UIButton, Ingredient); 10],
+    pub trackPos1: [(f32,f32,bool); 8],
+    pub trackPos2: [(f32,f32,bool); 8],
+    pub ingredPos1: [(UIButton, Ingredient); 8],
+    pub ingredPos2: [(UIButton, Ingredient); 8],
     //Ingredient list for the day
     pub ingredList: Vec<Ingredient>,
 }
@@ -15,44 +17,65 @@ pub struct Track {
 impl Track {
     pub fn new() -> Self {
         Self {
-            //currTrack: Vec<Ingredient>, list: Vec<Ingredient>
-            // trackList: currTrack,
-            // ingredList: list,
-            trackPos: [
-                //x Position, y Position, check if reached lowest or highest point in track, check if it reached end of screen
-                (0.0,70.0,false,false),
-                (0.0,70.0,false,false),
-                (0.0,70.0,false,false),
-                (0.0,70.0,false,false),
-                (0.0,70.0,false,false),
-                (0.0,70.0,false,false),
-                (0.0,70.0,false,false),
-                (0.0,70.0,false,false),
-                (0.0,70.0,false,false),
-                (0.0,70.0,false,false)
+            //x Position, y Position, check if reached lowest or highest point in track, check if it reached end of screen
+            trackPos1: [
+                (0.0,100.0,false),
+                (0.0,100.0,false),
+                (0.0,100.0,false),
+                (0.0,100.0,false),
+                (0.0,100.0,false),
+                (0.0,100.0,false),
+                (0.0,100.0,false),
+                (0.0,100.0,false),
             ],
-            ingredPos: [
-                //x Position, y Position, ingredient on the track
-                (UIButton::new("ing", (0.0, 70.0,10.0,10.0), false), 
+            //lower track
+            trackPos2: [
+                (250.0,30.0,false),
+                (250.0,30.0,false),
+                (250.0,30.0,false),
+                (250.0,30.0,false),
+                (250.0,30.0,false),
+                (250.0,30.0,false),
+                (250.0,30.0,false),
+                (250.0,30.0,false),
+            ],
+            //x Position, y Position, ingredient on the track
+            ingredPos1: [
+                (UIButton::new("ing", (0.0,100.0,10.0,10.0), false), 
                 Ingredient::new(IngredientType::Sweet, "empty")),
-                (UIButton::new("ing", (0.0, 70.0,10.0,10.0), false), 
+                (UIButton::new("ing", (0.0,100.0,10.0,10.0), false), 
                 Ingredient::new(IngredientType::Sweet, "Sugar")),
-                (UIButton::new("ing", (0.0, 70.0,10.0,10.0), false), 
+                (UIButton::new("ing", (0.0,100.0,10.0,10.0), false), 
                 Ingredient::new(IngredientType::Sweet, "empty")),
-                (UIButton::new("ing", (0.0, 70.0,10.0,10.0), false), 
+                (UIButton::new("ing", (0.0,100.0,10.0,10.0), false), 
                 Ingredient::new(IngredientType::Sweet, "empty")),
-                (UIButton::new("ing", (0.0, 70.0,10.0,10.0), false), 
+                (UIButton::new("ing", (0.0,100.0,10.0,10.0), false), 
                 Ingredient::new(IngredientType::Sweet, "empty")),
-                (UIButton::new("ing", (0.0, 70.0,10.0,10.0), false), 
+                (UIButton::new("ing", (0.0,100.0,10.0,10.0), false), 
                 Ingredient::new(IngredientType::Sweet, "empty")),
-                (UIButton::new("ing", (0.0, 70.0,10.0,10.0), false), 
+                (UIButton::new("ing", (0.0,100.0,10.0,10.0), false), 
                 Ingredient::new(IngredientType::Sweet, "Sugar")),
-                (UIButton::new("ing", (0.0, 70.0,10.0,10.0), false), 
+                (UIButton::new("ing", (0.0,100.0,10.0,10.0), false), 
                 Ingredient::new(IngredientType::Sweet, "Sugar")),
-                (UIButton::new("ing", (0.0, 70.0,10.0,10.0), false), 
+            ],
+            //lower track
+            ingredPos2: [
+                (UIButton::new("ing", (250.0,30.0,10.0,10.0), false), 
                 Ingredient::new(IngredientType::Sweet, "empty")),
-                (UIButton::new("ing", (0.0, 70.0,10.0,10.0), false), 
-                Ingredient::new(IngredientType::Sweet, "empty"))
+                (UIButton::new("ing", (250.0,30.0,10.0,10.0), false), 
+                Ingredient::new(IngredientType::Sweet, "empty")),
+                (UIButton::new("ing", (250.0,30.0,10.0,10.0), false), 
+                Ingredient::new(IngredientType::Sweet, "empty")),
+                (UIButton::new("ing", (250.0,30.0,10.0,10.0), false), 
+                Ingredient::new(IngredientType::Sweet, "Sugar")),
+                (UIButton::new("ing", (250.0,30.0,10.0,10.0), false), 
+                Ingredient::new(IngredientType::Sweet, "empty")),
+                (UIButton::new("ing", (250.0,30.0,10.0,10.0), false), 
+                Ingredient::new(IngredientType::Sweet, "empty")),
+                (UIButton::new("ing", (250.0,30.0,10.0,10.0), false), 
+                Ingredient::new(IngredientType::Sweet, "empty")),
+                (UIButton::new("ing", (250.0,30.0,10.0,10.0), false), 
+                Ingredient::new(IngredientType::Sweet, "Sugar")),
             ],
             ingredList: Vec::new(),
         }

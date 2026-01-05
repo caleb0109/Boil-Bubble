fae8be5535d0f585544678d21c3ac3b0448ba316
saki <sakurakitsune1120@gmail.com>
2026-01-05 13:03:06 -0500

    im gonna kms if you dont fucking merge

--- boilbubble/src/customer/customer.rs
+++ boilbubble/src/customer/customer.rs
@@ -25,7 +25,16 @@ impl Customer {
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
 =======
+=======
+>>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
 =======
